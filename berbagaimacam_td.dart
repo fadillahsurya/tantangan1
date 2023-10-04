153e49e89ae42f90a900e5bd3919755da899d397
fadillahsurya <103927620+fadillahsurya@users.noreply.github.com>
2023-10-04 14:28:13 +0700

    Add files via upload

---
+++ berbagaimacam_td.dart
@@ -0,0 +1,11 @@
+void main() {
+  List<int> datalistku = [4, 7, 8, 2, 6, 3, 6, 78];
+
+  // Mengurutkan daftar secara ascending
+  datalistku.sort();
+
+  // Menambahkan angka 109 ke dalam daftar
+  datalistku.add(109);
+
+  print('Daftar yang telah diurutkan dan ditambahkan 109: $datalistku');
+}
